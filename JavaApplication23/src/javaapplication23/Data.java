7575a83b1608002c42f1724fb5e53f6e647ab2c9
nguyentri <14520994@gm.uit.edu.vn>
2017-05-06 09:52:40 +0700

    Data Class

--- JavaApplication23/src/javaapplication23/Data.java
+++ JavaApplication23/src/javaapplication23/Data.java
@@ -5,10 +5,32 @@
  */
 package javaapplication23;
 
+import java.util.Vector;
+
 /**
  *
  * @author Minh Tri
  */
 public class Data {
-    
+    private static Vector giathiet = new Vector();
+    private static Vector ketluan = new Vector();
+    public Data(Vector _giathiet, Vector _ketluan)
+    {
+        for(int i = 0; i < _giathiet.size(); i++)
+        {
+            giathiet.add(_giathiet.get(i));
+        }
+        for(int j = 0; j < _ketluan.size(); j++)
+        {
+            ketluan.add(_ketluan.get(j));
+        }
+    }
+    public Vector get_giathiet()
+    {
+        return giathiet;
+    }
+    public Vector get_ketluan()
+    {
+        return giathiet;
+    }   
 }
